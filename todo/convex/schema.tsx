ba16a4cf7a6e293800d83e55d0735983f54df9d0
thomxz-cod <thomazerick35@gmail.com>
2026-03-25 13:50:19 -0300

    delete

--- todo/convex/schema.tsx
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server"
-
-import { v } from "convex/values"
-export default defineSchema({
-    todo : defineTable ({
-        text: v.string(),
-        isCompleted: v.boolean()
-    })
-})
